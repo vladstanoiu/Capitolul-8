db14e76af3020ef957bc27cafcf2c89b43192944
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2020-01-28 00:44:08 +0200

    Create Roman.h

---
+++ Conferinta/include/Roman.h
@@ -0,0 +1,19 @@
+#ifndef ROMAN_H
+#define ROMAN_H
+
+#include <Participant.h>
+
+
+class Roman : public Participant
+{
+    public:
+        Roman();
+        Limba Vorbeste();
+        virtual ~Roman();
+
+    protected:
+
+    private:
+};
+
+#endif // ROMAN_H

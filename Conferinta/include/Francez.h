be13786fa9ae9b426e5da010f7b11211445542ef
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2020-01-28 00:44:23 +0200

    Create Francez.h

---
+++ Conferinta/include/Francez.h
@@ -0,0 +1,19 @@
+#ifndef FRANCEZ_H
+#define FRANCEZ_H
+
+#include <Participant.h>
+
+
+class Francez : public Participant
+{
+    public:
+        Francez();
+        Limba Vorbeste();
+        virtual ~Francez();
+
+    protected:
+
+    private:
+};
+
+#endif // FRANCEZ_H

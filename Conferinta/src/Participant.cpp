e01da6c49030eb58cecbfd2fa3aff45d5a2cf49e
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2020-01-28 00:43:50 +0200

    Create Participant.cpp

---
+++ Conferinta/src/Participant.cpp
@@ -0,0 +1,15 @@
+#include "Participant.h"
+
+Participant::Participant()
+{
+    //ctor
+}
+Limba Participant::Vorbeste()
+{
+    return Limba::engleza;
+}
+
+Participant::~Participant()
+{
+    //dtor
+}

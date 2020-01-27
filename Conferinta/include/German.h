6583d55ada976e280fce0ee362c5a44fe3465c52
vladstanoiu <55551799+vladstanoiu@users.noreply.github.com>
2020-01-28 00:44:18 +0200

    Create German.h

---
+++ Conferinta/include/German.h
@@ -0,0 +1,19 @@
+#ifndef GERMAN_H
+#define GERMAN_H
+
+#include <Participant.h>
+
+
+class German : public Participant
+{
+    public:
+        German();
+        Limba Vorbeste();
+        virtual ~German();
+
+    protected:
+
+    private:
+};
+
+#endif // GERMAN_H
